55c969e98cfec79b785a91605430f9fbb8263232
Lucas Buffet <lebuffet02@gmail.com>
2023-04-10 00:24:38 -0300

    Inicializando o projeto oauth

---
+++ oauth/src/test/java/br/com/grupo2/backend/api/OauthApplicationTest.java
@@ -0,0 +1,15 @@
+package br.com.grupo2.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class OauthApplicationTest {
+
+    @Test
+    void testaMetodoMain() {
+        OauthApplication.main(new String[] {});
+    }
+}
---
+++ oauth/src/test/java/br/com/grupo2/backend/api/OauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.grupo2.backend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
