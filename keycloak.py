c41baea1233072b17879c229ed4f8a18f565be06
Pedro <phbattistellavieira@gmail.com>
2023-04-19 05:46:25 -0300

    Separated get_user() function
    updated requirements and divided the get_user() function into two, so we can get the full user info os just some of the most relevant info

--- keycloak.py
+++ keycloak.py
@@ -1,6 +1,8 @@
+from collections import OrderedDict
+import json
 import os
 import re
-from flask import Flask, request, jsonify, render_template, redirect
+from flask import Flask, Response, request, jsonify, render_template
 import requests
 
 D_SERVER_URL = 'https://keycloak:8080/auth'
@@ -11,6 +13,51 @@ TOKEN_URL = 'http://localhost:8090/auth/realms/Construc-sw-2023-1/protocol/openi
 USERS_URL = "http://localhost:8090/auth/admin/realms/Construc-sw-2023-1/users/"
 
 app = Flask(__name__)
+
+def get_full_user(user_id):
+    token = request.headers.get('Authorization')
+    url = D_USERS_URL + user_id
+    headers = {'Authorization': token}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 400:
+        return jsonify({'error_code': 'OA-400','error_description': 'Bad Request: Request structure error'}), 400
+    if response.status_code == 401:
+        return jsonify({'error_code': 'OA-401','error_description': 'Unauthorized: Invalid Token or username/password'}), 401
+    if response.status_code == 403:
+        return jsonify({'error_code': 'OA-403','error_description': 'Forbidden: Missing the necessary roles or privilages'}), 403
+    if response.status_code == 404:
+        return jsonify({'error_code': 'OA-404','error_description': 'Not Found: User not Found'}), 404
+    return jsonify(response.json()), 200
+
+def get_resumed_user(user_id):
+    token = request.headers.get('Authorization')
+    url = D_USERS_URL + user_id
+    headers = {'Authorization': token}
+    response = requests.get(url, headers=headers)
+    id = response.json()['id']
+    username = response.json()['username']
+    first_name = response.json()['firstName']
+    last_name = response.json()['lastName']
+    email = response.json()['email']
+    enabled = response.json()['enabled']
+    user = OrderedDict(
+        id=id,
+        username=username,
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        enabled=enabled
+    )
+    if response.status_code == 400:
+        return jsonify({'error_code': 'OA-400','error_description': 'Bad Request: Request structure error'}), 400
+    if response.status_code == 401:
+        return jsonify({'error_code': 'OA-401','error_description': 'Unauthorized: Invalid Token or username/password'}), 401
+    if response.status_code == 403:
+        return jsonify({'error_code': 'OA-403','error_description': 'Forbidden: Missing the necessary roles or privilages'}), 403
+    if response.status_code == 404:
+        return jsonify({'error_code': 'OA-404','error_description': 'Not Found: User not Found'}), 404
+    return Response(json.dumps(user), status=200, mimetype='application/json')
+
 
 @app.route('/')
 def home():
@@ -40,16 +87,6 @@ def create_user():
     headers = {'Content-Type': 'application/json', 'Authorization': token}
     payload = request.get_json()
     email = payload.get('email')
-    print(email)
-    # python não tankou o regex
-    #
-    #     if not re.match(r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:
-    # [\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\
-    # x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*
-    # [a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?
-    # [0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\
-    # [\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
-    # , email):
     if not email or not re.match(r"[^@]+@[^@]+\.[^@]+", email):
        return jsonify({'error_code': 'OA-400','error_description': 'Bad Request: Missing email or badly formated email'}), 400
     response = requests.post(url, headers=headers, json=payload)
@@ -61,7 +98,7 @@ def create_user():
             'location': user_info['location'].split('/')[-1]
         }
     print(new_user)
-    return redirect(D_USERS_URL + new_user['location'], code=302)
+    return get_full_user(new_user['location']), 201
 
 @app.route('/users', methods=['GET'])
 def get_users():
@@ -71,21 +108,10 @@ def get_users():
     response = requests.get(url, headers=headers)
     return jsonify(response.json()), 200
 
+# WORKING AS INTENDED
 @app.route('/users/<user_id>', methods=['GET'])
-def get_user(user_id):
-    token = request.headers.get('Authorization')
-    url = D_USERS_URL + user_id
-    headers = {'Authorization': token}
-    response = requests.get(url, headers=headers)
-    if response.status_code == 400:
-        return jsonify({'error_code': 'OA-400','error_description': 'Bad Request: Request structure error'}), 400
-    if response.status_code == 401:
-        return jsonify({'error_code': 'OA-401','error_description': 'Unauthorized: Invalid Token or username/password'}), 401
-    if response.status_code == 403:
-        return jsonify({'error_code': 'OA-403','error_description': 'Forbidden: Missing the necessary roles or privilages'}), 403
-    if response.status_code == 404:
-        return jsonify({'error_code': 'OA-404','error_description': 'Not Found: User not Found'}), 404
-    return jsonify(response.json()), 200
+def use_get_user(user_id):
+    return get_resumed_user(user_id)
 
 @app.route('/users/<user_id>', methods=['PUT'])
 def put_user(user_id):
@@ -103,8 +129,10 @@ def put_user(user_id):
         return jsonify({'error_code': 'OA-403','error_description': 'Forbidden: Missing the necessary roles or privilages'}), 403
     if response.status_code == 404:
         return jsonify({'error_code': 'OA-404','error_description': 'Not Found: User not Found'}), 404
-    return jsonify({'success': True}), 200
+    return jsonify(get_resumed_user(user_id)), 200
     
+
+# ADD THE USER RETURN HERE ASWELL
 @app.route('/users/<user_id>', methods=['PATCH'])
 def patch_user(user_id):
     token = request.headers.get('Authorization')
